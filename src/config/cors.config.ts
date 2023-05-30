1a7bf88ee22763f3d33c18c1a8daf9ece0128029
Aham Kingsley <kingsleyaham6@gmail.com>
2023-05-30 06:12:26 +0100

    fix: updated cors.config file

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -2,6 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // const allowedOrigins: Array<string> = (process.env.ALLOWED_ORIGIN as string).split(",");
-const allowedOrigins: Array<string> = ["http://localhost:3000"];
+const allowedOrigins: Array<string> = [
+  "http://localhost:3000",
+  "https://fcsl-reports-admin.vercel.app",
+];
 
 export default allowedOrigins;
